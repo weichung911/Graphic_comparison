ce7abd7e5769e860bef9d11242a73eb03540348a
weichung911 <weichung880911@gmail.com>
2023-09-03 12:06:36 +0800

    add dhash

--- Hash_function.py
+++ Hash_function.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as np
 
 def average_hash(img, hash_size=8):
     A_hash = cv2.resize(img,(hash_size,hash_size),interpolation=cv2.INTER_CUBIC)
     A_hash = cv2.cvtColor(A_hash,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('2',A_hash)
+    cv2.imshow('A_hash',A_hash)
     cv2.waitKey(0)
     avg_pixel = A_hash.mean()
     
@@ -14,3 +15,27 @@ def average_hash(img, hash_size=8):
 def hamming_distance(hash1, hash2):
     # Calculate the Hamming distance between two hashes
     return sum(bit1 != bit2 for bit1, bit2 in zip(hash1, hash2))
+
+def perceptual_Hash(img, hash_size=32):
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized_image = cv2.resize(gray_image, (hash_size, hash_size),interpolation=cv2.INTER_CUBIC)
+    dct_image = cv2.dct(np.float32(resized_image))
+    dct_image = dct_image[:8, :8]
+    cv2.imshow('dct_image',dct_image)
+    cv2.waitKey(0)
+    mean_value = np.mean(dct_image)
+    hash_value = "".join(['1' if pixel > mean_value else '0' for pixel in dct_image.flatten()])
+    
+    return hash_value
+
+def difference_Hash(img):
+    image = cv2.resize(img,(9,8),interpolation=cv2.INTER_CUBIC )
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    dhash_str = ''
+    for i in range(8):
+        for j in range(8):
+            if gray[i,j]>gray[i, j+1]:
+                dhash_str = dhash_str + '1'
+            else:
+                dhash_str = dhash_str + '0'
+    return dhash_str
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from Hash_function import *
 
 img = cv2.imread('test01.png')
 a_point = (0,0)
-b_point = (0,0)
+b_point = (12,0)
 size = 128
 
 a_img = img[a_point[1]:a_point[1]+size,a_point[0]:a_point[0]+size]
@@ -11,8 +11,13 @@ b_img = img[b_point[1]:b_point[1]+size,b_point[0]:b_point[0]+size]
 cv2.imshow('1',a_img)
 cv2.imshow('2',b_img)
 cv2.waitKey(0)
-hash_value1 = average_hash(a_img)
+# hash_value1 = average_hash(a_img)
+# print(hash_value1)
+# hash_value2 = average_hash(b_img)
+# print(hash_value2)
+# print(hamming_distance(hash_value1,hash_value2))
+hash_value1 = difference_Hash(a_img)
 print(hash_value1)
-hash_value2 = average_hash(b_img)
+hash_value2 = difference_Hash(b_img)
 print(hash_value2)
 print(hamming_distance(hash_value1,hash_value2))
